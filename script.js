765d78d95af4318cb796e3c39cc5c549d8af368a
julia-lika <julia.ishikawa@sou.inteli.edu.br>
2024-02-16 18:50:06 -0300

    colocando os console.log()

--- script.js
+++ script.js
@@ -51,17 +51,21 @@ function update() {
     if (passarinho.directionX === 'right' && passarinho.x < 700) {
         passarinho.x += 5;
         passarinho.setFlip(false, false);
+        console.log('Verifica se a direção horizontal é direita e se a posição x do passarinho é menor que 700. Se for verdadeiro, move o passarinho para a direita (passarinho.x += 5) e o mantém virado para a direita (passarinho.setFlip(false, false)).')
     } else if (passarinho.directionX === 'right' && passarinho.x === 700) {
         passarinho.directionX = 'left';
         passarinho.setFlip(true, false);
+        console.log('Se a posição x do passarinho for exatamente 700, muda a direção horizontal para esquerda e o vira para a esquerda (passarinho.setFlip(true, false)).')
     }
 
     if (passarinho.directionX === 'left' && passarinho.x > 100) {
         passarinho.x -= 5;
         passarinho.setFlip(true, false);
+        console.log('Verifica se a direção horizontal é esquerda e se a posição x do passarinho é maior que 100. Se for verdadeiro, move o passarinho para a esquerda (passarinho.x -= 5) e o mantém virado para a esquerda (passarinho.setFlip(true, false)).')
     } else if (passarinho.directionX === 'left' && passarinho.x === 100) {
         passarinho.directionX = 'right';
         passarinho.setFlip(true, false);
+        console.log('Se a posição x do passarinho for exatamente 100, muda a direção horizontal para direita e o vira para a direita (passarinho.setFlip(true, false)).')
     }
 
     // Movimento vertical
@@ -71,13 +75,17 @@ function update() {
 
     if (passarinho.directionY === 'down' && passarinho.y < 500) {
         passarinho.y += 5;
+        console.log('Verifica se a direção vertical é baixo e se a posição y do passarinho é menor que 500. Se for verdadeiro, move o passarinho para baixo (passarinho.y += 5).')
     } else if (passarinho.directionY === 'down' && passarinho.y === 500) {
         passarinho.directionY = 'up';
+        console.log('Se a posição y do passarinho for exatamente 500, muda a direção vertical para cima.')
     }
 
     if (passarinho.directionY === 'up' && passarinho.y > 100) {
         passarinho.y -= 5;
+        console.log('Verifica se a direção vertical é cima e se a posição y do passarinho é maior que 100. Se for verdadeiro, move o passarinho para cima (passarinho.y -= 5).')
     } else if (passarinho.directionY === 'up' && passarinho.y === 100) {
         passarinho.directionY = 'down';
+        console.log('Se a posição y do passarinho for exatamente 100, muda a direção vertical para baixo.')
     }
 }
